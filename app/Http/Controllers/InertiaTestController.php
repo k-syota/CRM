992a03c66f2326868d531feea0327cd4ab07f07c
Syota Nishimura <syota4723@gmail.com>
2022-11-28 16:31:17 +0900

    :sparkles: フラッシュメッセージ

--- app/Http/Controllers/InertiaTestController.php
+++ app/Http/Controllers/InertiaTestController.php
@@ -36,6 +36,8 @@ class InertiaTestController extends Controller
         $inertiaTest->title = $request->title;
         $inertiaTest->content = $request->content;
         $inertiaTest->save();
-        return to_route('inertia.index');
+        return to_route('inertia.index')->with([
+            'message' => '登録完了しました'
+        ]);
     }
 }
--- resources/js/Pages/Inertia/index.vue
+++ resources/js/Pages/Inertia/index.vue
@@ -3,7 +3,12 @@ import {Link} from '@inertiajs/inertia-vue3'
 </script>
 
 <template>
-    Index<br>
-    <a href="/">aタグでWelcomeに移動</a><br>
-    <Link href="/">LinkでWelcomeに移動</Link>
+    <div class="text-center">
+        Index<br>
+        <div class="my-4 bg-blue-300" v-if="$page.props.flash.message">
+            {{ $page.props.flash.message }}
+        </div>
+        <a href="/">aタグでWelcomeに移動</a><br>
+        <Link href="/">LinkでWelcomeに移動</Link>
+    </div>
 </template>
