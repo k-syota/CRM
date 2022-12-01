2ca2c8a2aa80fd9cb37abb6e805a69d7b26c48ad
Syota Nishimura <syota4723@gmail.com>
2022-12-01 16:55:41 +0900

    :adhesive_bandage: 新規登録ページの修正

--- resources/js/Pages/Items/Create.vue
+++ resources/js/Pages/Items/Create.vue
@@ -45,7 +45,7 @@ const storeItem = () => {
                                         </div>
                                         <div class="p-2 w-full">
                                             <div class="relative">
-                                                <label for="memo" class="leading-7 text-sm text-gray-600">Memo</label>
+                                                <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
                                                 <textarea id="memo" name="memo"
                                                 v-model="form.memo"
                                                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
@@ -60,7 +60,7 @@ const storeItem = () => {
                                             </div>
                                         </div>
                                         <div class="p-2 w-full">
-                                            <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+                                            <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">商品登録</button>
                                         </div>
                                     </div>
                                 </div>
