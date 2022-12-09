b4999dffecf77f712a71a6d4dc0d7cd40b799ba2
Syota Nishimura <syota4723@gmail.com>
2022-12-09 22:38:49 +0900

    :seedling: ItemPurchaseのダミーデータを作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Purchase;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,8 +21,16 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class
         ]);
         \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
+        // \App\Models\Purchase::factory(100)->create();
 
+        $items = \App\Models\Item::all();
+        Purchase::factory(100)->create()
+        //useで関数の外にある$itemを使用できるようにしている
+        ->each(function(Purchase $purchase) use ($items){
+            $purchase->items()->attach(
+                $items->random(rand(1,3))->pluck('id')->toArray(),['quantity' => rand(1,5)]
+            );
+        });
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
