941f0efd8a28029e63a819bf8ed76a4f80c33b71
Syota Nishimura <syota4723@gmail.com>
2023-01-18 22:59:44 +0900

    :sparkles: 分析するデータの期間を設定する

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class AnalysisController extends Controller
 {
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
     public function index()
     {
+
+        $startDate = '2023-01-17';
+        $endDate = '2023-01-31';
+
+        $period = Order::betweenDate($startDate, $endDate)->groupBy('id')
+        ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at') ->orderBy('created_at')
+        ->paginate(50);
+
+        dd($period);
+
         return Inertia::render('Analysis');
     }
 }
---
+++ app/Models/Order.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\Subtotal;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new Subtotal);
+    }
+
+    public function scopeBetweenDate($query, $startDate = null, $endDate = null)
+    {
+    if(is_null($startDate) && is_null($endDate))
+    { return $query; }
+    if(!is_null($startDate) && is_null($endDate))
+    { return $query->where('created_at', ">=", $startDate); }
+    if(is_null($startDate) && !is_null($endDate))
+    {
+    return $query->where('created_at', '<=', $endDate);
+    }
+    if(!is_null($startDate) && !is_null($endDate))
+    {
+    return $query->where('created_at', ">=", $startDate)
+    ->where('created_at', '<=', $endDate);
+    }
+    }
+}
