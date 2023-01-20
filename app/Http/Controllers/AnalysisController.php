2ddc80619de2246db8f6cbe1a760eeaf05f74be3
Syota Nishimura <syota4723@gmail.com>
2023-01-20 22:47:47 +0900

    :sparkles: 日別売上のクエリ作成、取得確認

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class AnalysisController extends Controller
@@ -16,14 +17,24 @@ class AnalysisController extends Controller
     public function index()
     {
 
-        $startDate = '2023-01-17';
+        $startDate = '2023-01-01';
         $endDate = '2023-01-31';
 
-        $period = Order::betweenDate($startDate, $endDate)->groupBy('id')
-        ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at') ->orderBy('created_at')
-        ->paginate(50);
+        // $period = Order::betweenDate($startDate, $endDate)->groupBy('id')
+        // ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at') ->orderBy('created_at')
+        // ->paginate(50);
 
-        // dd($period);
+        $subQuery = Order::betweenDate($startDate,$endDate)
+        ->where('status', true)->groupBy('id')
+        ->selectRaw('id, SUM(subtotal) as totalPerPurchase,
+        DATE_FORMAT(created_at, "%Y%m%d") as date');
+
+        $data = DB::table($subQuery)
+        ->groupBy('date')
+        ->selectRaw('date, SUM(totalPerPurchase) as total')
+        ->get();
+
+        // dd($data);
 
         return Inertia::render('Analysis');
     }
