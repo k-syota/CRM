29e10fb43bddbdc9ea29e67ffbabf0aac00c0252
Syota Nishimura <syota4723@gmail.com>
2023-01-21 18:56:24 +0900

    :sparkles: 日付売上をテーブル表示

--- resources/js/Pages/Analysis.vue
+++ resources/js/Pages/Analysis.vue
@@ -16,6 +16,8 @@ const form = reactive({
     type: 'perDay'
 })
 
+const data = reactive({})
+
 const getDate = async () => {
     try{
         await axios.get('/api/analysis/',{
@@ -26,7 +28,7 @@ const getDate = async () => {
             }
         })
         .then(res => {
-            console.log(res.data)
+            data.data = res.data.data;
         })
     }catch(e){
         console.log(e.message)
@@ -53,6 +55,22 @@ const getDate = async () => {
                                 分析する
                             </button>
                         </form>
+                        <div v-if="data.data" class="lg:w-2/3 w-full mx-auto overflow-auto">
+                            <table class="table-auto w-full text-left whitespace-no-wrap">
+                                <thead>
+                                <tr>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">金額</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="item in data.data" :key="item.date">
+                                    <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.date }}</td>
+                                    <td class="px-4 py-3 border-b-2 border-gray-200">{{ item.total }}</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
