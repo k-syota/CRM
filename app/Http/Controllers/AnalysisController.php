3e63451e5cb433c71e099ea0057fc16eb542f7be
Syota Nishimura <syota4723@gmail.com>
2023-01-23 22:54:08 +0900

    :sparkles: 連番を振って必要な変数に設定

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -27,10 +27,26 @@ class AnalysisController extends Controller
         $subQuery = DB::table($subQuery)
         ->groupBy('customer_id')
         ->selectRaw('customer_id, customer_name, sum(totalPerPurchase) as total')
-        ->orderBy('total', 'desc')
-        ->get();
+        ->orderBy('total', 'desc');
 
-        dd($subQuery);
+        DB::statement('set @row_num = 0;');
+        $subQuery = DB::table($subQuery) ->selectRaw('
+        @row_num:= @row_num+1 as row_num, customer_id,
+        customer_name, total');
+
+        $count = DB::table($subQuery)->count();
+        $total = DB::table($subQuery)
+        ->selectRaw('sum(total) as total')->get(); $total = $total[0]->total; // 構成比用
+        $decile = ceil($count / 10); // 10分の1の件数を変数に入れる
+        $bindValues = []; $tempValue = 0;
+
+        for($i = 1; $i <= 10; $i++) {
+            array_push($bindValues, 1 + $tempValue);
+            $tempValue += $decile;
+            array_push($bindValues, 1 + $tempValue);
+        }
+
+        dd($count,$decile,$bindValues);
 
         return Inertia::render('Analysis');
     }
