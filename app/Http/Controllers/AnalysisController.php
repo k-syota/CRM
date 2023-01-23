08ad60fa4bf328765faaddfa1a6fc672f113c613
Syota Nishimura <syota4723@gmail.com>
2023-01-23 22:44:04 +0900

    :sparkles: デシル分析の実装１
    会員ごとに纏めて金額順でsortする

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -20,21 +20,17 @@ class AnalysisController extends Controller
         $startDate = '2023-01-01';
         $endDate = '2023-01-31';
 
-        // $period = Order::betweenDate($startDate, $endDate)->groupBy('id')
-        // ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at') ->orderBy('created_at')
-        // ->paginate(50);
+        $subQuery = Order::betweenDate($startDate, $endDate)
+        ->groupBy('id')
+        ->selectRaw('id, customer_id, customer_name, SUM(subtotal) as totalPerPurchase');
 
-        $subQuery = Order::betweenDate($startDate,$endDate)
-        ->where('status', true)->groupBy('id')
-        ->selectRaw('id, SUM(subtotal) as totalPerPurchase,
-        DATE_FORMAT(created_at, "%Y%m%d") as date');
-
-        $data = DB::table($subQuery)
-        ->groupBy('date')
-        ->selectRaw('date, SUM(totalPerPurchase) as total')
+        $subQuery = DB::table($subQuery)
+        ->groupBy('customer_id')
+        ->selectRaw('customer_id, customer_name, sum(totalPerPurchase) as total')
+        ->orderBy('total', 'desc')
         ->get();
 
-        // dd($data);
+        dd($subQuery);
 
         return Inertia::render('Analysis');
     }
